f420e816dd2f19c830181cf440c1215c8d45ecf6
werascals <werascals09@gmail.com>
2022-05-12 16:59:50 +0530

    .

--- js/bestof.js
+++ js/bestof.js
@@ -13,20 +13,16 @@ fetch("json/review.json").then((data) => {
     //     <p>${info.tittle}</p>
     // </div>`;
     for (let i = 0; i < 1; i++) {
-        // document.getElementsByClassName("copper").innerHTML = jadu[i].tittle
         document.getElementById("copper1").innerHTML = jadu[i].tittle
     }
     for (let i = 1; i < 2; i++) {
-        // document.getElementsByClassName("copper").innerHTML = jadu[i].tittle
         document.getElementById("copper2").innerHTML = jadu[i].tittle
     }
-    // })
+
     for (let i = 1; i < 3; i++) {
-        // document.getElementsByClassName("copper").innerHTML = jadu[i].tittle
         document.getElementById("copper3").innerHTML = jadu[i].tittle
     }
     for (let i = 1; i < 4; i++) {
-        // document.getElementsByClassName("copper").innerHTML = jadu[i].tittle
         document.getElementById("copper4").innerHTML = jadu[i].tittle
     }
 }).catch((err) => { console.log(err) })
